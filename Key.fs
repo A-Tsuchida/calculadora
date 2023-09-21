22b7bd99a6c1c36f38ba53a3f0d00bf8371f47c3
Anderson Kurz <anderson_kurz@outlook.com>
2023-09-21 14:42:13 +0000

    extract datatypes to separate file

--- Key.fs
+++ Key.fs
@@ -1,32 +1,11 @@
 module Key
-
-type Number =
-| One   = 1
-| Two   = 2
-| Three = 3
-| Four  = 4
-| Five  = 5
-| Six   = 6
-| Seven = 7
-| Eigth = 8
-| Nine  = 9
-| Zero  = 0
-
-type Operation =
-| Multiply
-| Divide
-| Sum
-| Subtraction
-
-type KeyType =
-| Operation
-| Number
+open DataTypes
 
 type Key(face: string, kt: KeyType) as this =
   [<DefaultValue>] val mutable private onPress: KeyType -> unit
 
   do
-    this.onPress <- (fun kt -> kt |> ignore)
+    this.onPress <- fun _ -> ignore ()
 
   member public _.Face with get() = face
   member public _.Type with get() = kt
