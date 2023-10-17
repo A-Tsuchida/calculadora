8bc10593f3352a3c863123c134a60065da68cbe6
Anderson Augusto Kurz <33233355+A-Tsuchida@users.noreply.github.com>
2023-10-17 20:38:20 -0300

    Finish display implementation

--- calculator/Display.fs
+++ calculator/Display.fs
@@ -1,14 +1,15 @@
 module Display
 open DataTypes
 
-type DataHandler = float -> unit
+
+type DataHandler = Number -> unit
+type DecimalHandler = unit -> unit
 type SignalHandler = bool -> unit
 type ErrorHandler = bool -> unit
 
 type Display () as this =
   [<DefaultValue>] val mutable private number: Number List
-  [<DefaultValue>] val mutable private hasDecimal: bool
-  [<DefaultValue>] val mutable private decimal: Number List
+  [<DefaultValue>] val mutable private decimal: Number List option
   [<DefaultValue>] val mutable private signal: bool
   [<DefaultValue>] val mutable private error: bool
 
@@ -16,18 +17,23 @@ type Display () as this =
     this.error <- false
     this.signal <- false
     this.number <- List.Empty
-    this.hasDecimal <- false
-    this.decimal <- List.Empty
+    this.decimal <- None
 
   member public x.Add (value: Number) =
-    if x.hasDecimal
-    then
-      x.decimal <- x.decimal @ [value]
-    else
-      x.number <- x.number @ [value]
+    let updateList (lst: Number list) =
+      match lst, value with
+      | Zero :: [], Zero -> lst
+      | _, _ -> value :: lst
+
+    match x.decimal with
+    | Some lst -> x.decimal <- Some (updateList lst)
+    | None -> x.number <- updateList x.number
+
+    this.Print ()
 
   member public x.SetDecimal () =
-    x.hasDecimal <- true
+    if this.decimal = None
+    then this.decimal <- Some []
 
   member public x.SetSignal state =
     x.signal <- state
@@ -35,6 +41,45 @@ type Display () as this =
   member public x.SetError state =
     x.error <- state
 
-  member private x.PrintDigit (digit: Number) =
+  member private x.RenderDigit (digit: Number) =
     match digit with
-    | One
+    | One -> "1"
+    | Two -> "2"
+    | Three -> "3"
+    | Four -> "4"
+    | Five -> "5"
+    | Six -> "6"
+    | Seven -> "7"
+    | Eigth -> "8"
+    | Nine -> "9"
+    | Zero -> "0"
+
+  member private x.Print () =
+    if this.error
+    then printf "\u001B[32m[E] "
+    else printf "\u001B[33m"
+    if this.signal
+    then printf "- "
+    let rec ``process`` (list: Number List) =
+      match list with
+      | current :: remain -> printf "%s" (x.RenderDigit current)
+                             ``process`` remain
+      | [] -> ()
+    x.number
+    |> List.rev
+    |> ``process``
+    match x.decimal with
+    | Some lst ->
+      printf "."
+      lst
+      |> List.rev
+      |> ``process``
+    | None -> ()
+    printfn "\u001B[39;49m"
+
+  member public x.Clear () =
+    x.number <- List.Empty
+    x.decimal <- None
+    x.signal <- false
+    x.error <- false
+    this.Print ()
