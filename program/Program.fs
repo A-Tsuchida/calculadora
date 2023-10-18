63f00abf6c2015897a1d712a7f174188ace14f17
Anderson Augusto Kurz <33233355+A-Tsuchida@users.noreply.github.com>
2023-10-17 21:46:45 -0300

    tests

--- program/Program.fs
+++ program/Program.fs
@@ -1,5 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open Calculator
+﻿open Calculator
 open Cpu
 open Keyboard
 open Display
@@ -11,3 +10,20 @@ keyboard.Add (Key ("1", KeyType.Number (Number.One)))
 
 let calc = Calculator (Cpu (), keyboard, Display ())
 
+let display = Display ()
+let cpu = Cpu ()
+cpu.SetNumberHandler (Some display.Add)
+cpu.SetDecimalHandler (Some display.SetDecimal)
+cpu.SetErrorHandler (Some display.SetError)
+cpu.SetSignalHandler (Some display.SetSignal)
+cpu.SetClearHandler (Some display.Clear)
+
+Control On |> cpu.Process
+Number Two |> cpu.Process
+Number Zero |> cpu.Process
+Operation Multiply |> cpu.Process
+Number Zero |> cpu.Process
+Control Decimal |> cpu.Process
+Number One |> cpu.Process
+Number One |> cpu.Process
+Control Equal |> cpu.Process
