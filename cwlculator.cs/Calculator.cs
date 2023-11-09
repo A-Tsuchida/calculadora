03a44f2c2fae2ac1b56690b85ed233edab7b6d3d
Anderson Augusto Kurz <33233355+A-Tsuchida@users.noreply.github.com>
2023-11-09 13:09:37 -0300

    Add main calculator class

---
+++ cwlculator.cs/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator;
+public class Calculator
+{
+    private Cpu cpu;
+
+    public IDisplay Display { get; }
+    public IKeyboard Keyboard { get; }
+
+    public Calculator(IDisplay display, IKeyboard keyboard, Cpu cpu)
+    {
+        Display = display;
+        Keyboard = keyboard;
+
+        this.cpu = cpu;
+    }
+}
