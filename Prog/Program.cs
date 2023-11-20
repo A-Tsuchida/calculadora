48b5a055ca322beb12d8db812e0dd2cb1b84d45b
Anderson Augusto Kurz <33233355+A-Tsuchida@users.noreply.github.com>
2023-11-20 16:03:21 -0300

    Add Prog project and Program.cs file

---
+++ Prog/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator;
+using Calculator.DataTypes;
+
+var dsp = new Display(8);
+var cpu = new Cpu();
+cpu.ClearEvent += dsp.Clear;
+cpu.DataEvent += dsp.AddNumber;
+cpu.DecimalEvent += dsp.SetDecimal;
+cpu.ErrorEvent += dsp.SetError;
+cpu.NegativeEvent += dsp.SetNegative;
+cpu.NumberEvent += dsp.Add;
+
+cpu.Process(new OpCode.Control(new Control.On()));
+cpu.Process(new OpCode.Number(new Number.Nine()));
+cpu.Process(new OpCode.Operation(new Operation.Division()));
+cpu.Process(new OpCode.Number(new Number.Three()));
+cpu.Process(new OpCode.Control(new Control.InvertSignal()));
+cpu.Process(new OpCode.Control(new Control.Equal()));
